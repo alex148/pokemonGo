f5e0c721a94364454ceae393f29addc88639f39d
Robin <robinboitemail@gmail.com>
2016-01-04 17:10:14 +0100

    Crud -> GETALL

--- app/src/main/java/com/pokemongo/dao/Crud.java
+++ app/src/main/java/com/pokemongo/dao/Crud.java
@@ -1,5 +1,7 @@
 package com.pokemongo.dao;
 
+import java.util.List;
+
 /**
  * Created by Alex on 18/12/2015.
  */
@@ -8,6 +10,6 @@ public interface Crud<T> {
     public long insert(T object);
     public void delete(T object);
     public void update(T object);
-    public T get(T object);
+    public List<T> getAll();
 
 }
